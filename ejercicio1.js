ed2015ce42abc64861c6c63c531840e83eb3afb8
Omar <oromero@unsa.edu.pe>
2023-05-16 22:03:32 -0500

    Ejercicio 1 implementado

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,9 +1,9 @@
 var button_get_day = document.getElementById("getDay");
-button_get_day.onclik = function (){
-    var value = document.getElementById("day").value;
-    var days = ['domingo', 'lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado'];
+button_get_day.onclick = function (){
+    var value = document.getElementById("day").value;	
+    var days = ['domingo','lunes','martes','miercoles','jueves','viernes','sabado'];
     var today = new Date();
     var result = document.getElementById("result_day");
-    result.text.Contex = days[value];
+    result.textContent =days[value];
     result.style.color = 'red';
 };
